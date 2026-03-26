f7ba82c24326a343c0ffacedf1f7c81769cca260
basantsaed2 <basantsaed718@gmail.com>
2026-03-26 11:58:18 +0200

    edit courier

--- src/Pages/Couriers/Couriers.jsx
+++ src/Pages/Couriers/Couriers.jsx
@@ -57,12 +57,12 @@ const Couriers = () => {
 
     if (loading) return <Loader />;
 
-    if (error && !error.includes("404")) {
-        return (
+    {
+        error && !error.includes("404") && (
             <div className="mb-4 p-4 bg-red-50 text-red-600 rounded-lg border border-red-100">
-                {t("Errorloadingcouriers")}: {error}
+                {t("Errorloadingbrands")}: {error}
             </div>
-        );
+        )
     }
 
     return (
